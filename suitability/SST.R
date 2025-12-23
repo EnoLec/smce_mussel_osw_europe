3484a4493197356ec006449eed1e9dfa0e15a1dc
Enora Lecordier <73999725+EnoLec@users.noreply.github.com>
2025-12-23 14:59:54 +0100

    Update SST.R

--- suitability/SST.R
+++ suitability/SST.R
@@ -33,23 +33,7 @@ XTmax_exclusion = function(x, y) {    # Takes the same pixels of fT and XTmax
   return(x)
 }
 
-# 1. Average sst 2019-2023 ----
-## DO NOT RUN AGAIN ##
-# coasline <-  read_sf("C:/Users/enora/OneDrive - hull.ac.uk/001_Enora_PhD/Data/Suitable_WF_GIS_study/MCE/GIS/Europe_coastline_shapefile/Europe_coastline_poly.shp")
-#
-# SST_files <- list.files(pattern = "^SST_20")                 # Select sst files in the folder 
-# SST_files <- lapply(SST_files, function(filename) {          # Open the files
-#   raster::brick(filename, varname = "analysed_sst")-272.15   #  Convert kelvin in Celsius
-# })
-# 
-# SST_stack <- stack(SST_files)                                # Stack the files together
-# temp <- mean(SST_stack,  na.rm=TRUE)                         # Average all the stack layers to give one raster
-# temp <- mask(temp, coastline, inverse=TRUE)                  # Mask the result with the coastline for a clean result
-#
-# writeRaster(temp,"SST_averaged_2019-2023.tif", overwrite=T)
-
-
-# 2. Compute fuzzy score ----
+# Compute fuzzy score ----
 SST_files <- list.files(pattern = "^SST_20") # List sst files in folder
 
 file_groups <- split(SST_files, cut(seq_along(SST_files), 5, labels = FALSE)) # Split the files into 5 groups for the 5 years
@@ -154,5 +138,6 @@ plot(SSP_change)
 
 
 writeRaster(SSP_change,"Bio-oracle_sst_anomalies.tif", overwrite=T)
+
 
 
