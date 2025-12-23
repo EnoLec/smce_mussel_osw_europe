52ec794f8799fc65e76b999a9a45788992675245
Enora Lecordier <73999725+EnoLec@users.noreply.github.com>
2025-12-23 15:32:16 +0100

    Update Thresholds_maker.R

--- suitability/rscripts/Thresholds_maker.R
+++ suitability/rscripts/Thresholds_maker.R
@@ -5,6 +5,7 @@
 # Here we combine CHL, SST, SSS and SPM suitability scores together
 # First, we are going to generate a suitability index of M. edulis growth for the whole of Europe (without feasibility mask applied), to allow a complete study of the region.
 # Then, we will aplpy the Feasiblity mask to identify the best areas.
+# Finally, we'll compute the suitability index (SI) trend by 2050.
 
 # Initialisation ####
 library(raster)
@@ -17,10 +18,10 @@ setwd("C:/Users/my/path/")
 
 # SMCE full Europe ----
 # Load the suitability score rasters for each factor
-sss_fuzzy <- raster("Thresholds/SSS/SSS_2019-2023_fuzzy.tif")
-sst_fuzzy <- raster("Thresholds/SST/SST_2019-2023_fuzzy.tif")
-chl_fuzzy <- raster("Thresholds/CHL/CHL_2019-2023_fuzzy.tif")
-spm_fuzzy <- raster("Thresholds/SPM/SPM_2019-2023_fuzzy.tif")
+sss_fuzzy <- raster("SSS_2019-2023_fuzzy.tif")
+sst_fuzzy <- raster("SST_2019-2023_fuzzy.tif")
+chl_fuzzy <- raster("CHL_2019-2023_fuzzy.tif")
+spm_fuzzy <- raster("SPM_2019-2023_fuzzy.tif")
 
 # Check their resolution
 res(sss_fuzzy)
@@ -50,33 +51,41 @@ writeRaster(SMCE_europe,"SMCE_full_europe_2019-2023.tif", overwrite=T)
 
 
 # SMCE feasible Europe ----
-feasibility <- raster("C:/Users/enora/OneDrive - hull.ac.uk/001_Enora_PhD/Data/Suitable_WF_GIS_study/MCE/feasibility/feasibility_mask.tif")
-SMCE_europe <- raster("Thresholds/SMCE_full_europe_2019-2023.tif")
-coast25NM <- read_sf("C:/Users/enora/OneDrive - hull.ac.uk/001_Enora_PhD/Data/Suitable_WF_GIS_study/MCE/GIS/Europe_coastline_shapefile/Europe_coastline_25NM_buffer.shp")
+# Now we apply the Feasiblity mask
+feasibility <- raster("feasibility_mask.tif")
+SMCE_europe <- raster("SMCE_full_europe_2019-2023.tif") # reload our full Europe SI if needed
 
+# Rescale the feasibility mask on the SI
 feasibility_res <- resample(feasibility, SMCE_europe, method="ngb")
 
+# Duplicate the full Europe SI to crop it with the mask
 SMCE_feasible <- SMCE_europe
 
+# Mask out the full Europe SI with the feasibilty mask
 SMCE_feasible[feasibility_res==0] <- NA
 
+# Check your results
 plot(SMCE_feasible)
 
-writeRaster(SMCE_feasible,"Thresholds/SMCE_feasible_europe_2019-2023.tif", overwrite=T)
+writeRaster(SMCE_feasible,"SMCE_feasible_europe_2019-2023.tif", overwrite=T)
 
 
 # SMCE future trend ----
+# We compute the SI the same way we did previously but this time we use a different SST data (mean SST 2010-2020 and mean SST 2040-2050) 
 ## SMCE 2010-2020 ----
-sst_present <- raster("Thresholds/SST/SST_2010-2020_fuzzy.tif")
+sst_present <- raster("SST_2010-2020_fuzzy.tif")
 
+# Again, use SSS resolution
 sst_present <- resample(sst_present, sss_fuzzy)
 
+# STack the factors together and compute the SI
 SMCE_stack_present <- stack(sst_present, chl, sss, spm)
 weights <- c(0.45,0.30,0.16,0.09)
 SMCE_europe_present <- calc(SMCE_stack_present, fun = function(x) sum(x * weights))
 
+                            
 ## SMCE 2040-2050 ----
-sst_future <- raster("Thresholds/SST/SST_2040-2050_fuzzy.tif")
+sst_future <- raster("SST_2040-2050_fuzzy.tif")
 sst_future <- resample(sst_future, sss_fuzzy)
 
 SMCE_stack_future <- stack(sst_future, chl, sss, spm)
@@ -84,35 +93,17 @@ weights <- c(0.45,0.30,0.16,0.09)
 SMCE_europe_future <- calc(SMCE_stack_future, fun = function(x) sum(x * weights))
 
 ## Compute trend ----
-### Absolute future trend ----
-SMCE_europe_trend <- SMCE_europe_future-SMCE_europe_present
-
-# writeRaster(SMCE_europe_trend,"Thresholds/SMCE_full_europe_absolute_trend.tif", overwrite=T)
-
-SMCE_feasible_trend <- SMCE_europe_trend
-
-SMCE_feasible_trend[feasibility_res==0] <- NA
-
-plot(SMCE_feasible_trend)
-writeRaster(SMCE_feasible_trend,"Thresholds/SMCE_feasible_europe_absolute_trend.tif", overwrite=T)
-
-### Relative future trend ----
 SMCE_europe_trend <- 100*(SMCE_europe_future-SMCE_europe_present)/SMCE_europe_present
 
+# Optional if your want the trend on the full extent of Europe
 # writeRaster(SMCE_europe_trend,"Thresholds/SMCE_full_europe_absolute_trend.tif", overwrite=T)
 
+# Generate an object to mask
 SMCE_feasible_trend <- SMCE_europe_trend
 
+# Mask the raster with the Feasibility mask
 SMCE_feasible_trend[feasibility_res==0] <- NA
 
-plot(SMCE_feasible_trend)
-writeRaster(SMCE_feasible_trend,"Thresholds/SMCE_feasible_europe_relative_trend.tif", overwrite=T)
-
-# ### From palmer et el 2020 ####
-# MCE_4.5 <- raster("Thresholds/SMCE_SSP4.5.tif")
-# MCE_8.5 <- raster("Thresholds/SMCE_SSP8.5.tif")
-# stability <- abs(MCE_8.5-MCE_4.5)/max(MCE_8.5,MCE_4.5)
-# 
-# writeRaster(stability,"Thresholds/stability_SSP.tif", overwrite=T)
-# 
+plot(SMCE_feasible_trend) # Check your results
+writeRaster(SMCE_feasible_trend,"SMCE_feasible_europe_relative_trend.tif", overwrite=T)
 
